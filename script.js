f342e923584d415063e368b83ae5e0328bd6ba3d
AndreCeara <cearacaetes@gmail.com>
2025-02-23 19:11:26 -0300

    criei portuguesadoalegre

---
+++ script.js
@@ -0,0 +1,23 @@
+function clickMenu() {
+    if(itens.style.display == 'block'){
+        itens.style.display = 'none'
+    } else{
+        itens.style.display = 'block'
+    }
+}
+
+function clickDetalhes(){
+    if(conteudo.style.display == 'block'){
+        conteudo.style.display = 'none'
+    } else{
+        conteudo.style.display = 'block'
+    }
+}
+
+function clickDetalhesTorneios(){
+    if(expandir.style.display == 'block'){
+        expandir.style.display = 'none'
+    } else{
+        expandir.style.display = 'block'
+    }
+}
